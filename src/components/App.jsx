541b8d3c1034732294c9b1d74c104f0703c49bd3
Evgen Fedorchenko <135177643+NayxieX@users.noreply.github.com>
2025-05-18 17:12:26 +0300

    Created some routes

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,6 +1,19 @@
-import { useState } from 'react'
-import './App.css'
+import "./App.css";
 
 function App() {
+  return (
+    <div>
+      <Routes>
+        <Route path="/" element={<Parfumes />} />
+        <Route path="/brands" element={<Brands />} />
+        <Route path="/skincare" element={<Skincare />} />
+        <Route path="/makeup" element={<Makeup />} />
+        <Route path="/haircare" element={<Haircare />} />
+        <Route path="/aromatherapy" element={<Aromatherapy />} />
+        <Route path="/candles" element={<Candles />} />
+        <Route path="*" element={<NotFound />} />
+      </Routes>
+    </div>
+  );
 }
-export default App
+export default App;
